4db032301a491ca6f60345019b81627ccddd4c3f
Ronda Bergman <rondabergman@hotmail.com>
2016-07-29 08:56:26 -0400

    Initital commit

--- WebAPICRUD/Controllers/ValuesController.cs
+++ WebAPICRUD/Controllers/ValuesController.cs
@@ -4,36 +4,61 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPICRUD.Models;
 
 namespace WebAPICRUD.Controllers
 {
     public class ValuesController : ApiController
     {
+        ProductRepository repository = new ProductRepository();
+
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<Product> GetAll()
         {
-            return new string[] { "value1", "value2" };
+            return repository.GetAll();
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public Product GetProduct(int id)
         {
-            return "value";
+            Product item = repository.Get(id);
+            if (item == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return item;
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll().Where(
+                p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public Product PostProduct(Product item)
         {
+            item = repository.Add(item);
+            return item;
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public void PutProduct(int id, Product item)
         {
+            item.Id = 5;
+            if (!repository.Update(item))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        public void DeleteProduct(int id)
         {
+            Product item = repository.Get(id);
+            if (item == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            repository.Remove(id);
         }
     }
 }
---
+++ WebAPICRUD/Models/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPICRUD.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        List<Product> products = new List<Product>();
+        private int _nextId = 1;
+
+        public ProductRepository()
+        {
+            Add(new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1.5M, InStock = true });
+            Add(new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M, InStock = true });
+            Add(new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M, InStock = false });
+            Add(new Product { Id = 4, Name = "Screwdriver", Category = "Hardware", Price = 6.99M, InStock = true });
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return products;
+        }
+
+        public Product Get(int id)
+        {
+            return products.Find(p => p.Id == id);
+        }
+
+        public Product Add(Product item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException();
+            }
+            item.Id = _nextId++;
+            products.Add(item);
+            return item;
+        }
+
+        public void Remove(int id)
+        {
+            products.RemoveAll(p => p.Id == id);
+        }
+
+        public bool Update(Product item)
+        {
+            if (item == null)
+                throw new ArgumentNullException();
+
+            int index = products.FindIndex(p => p.Id == item.Id);
+            if (index == -1)
+                return false;
+
+            products.RemoveAt(index);
+            products.Add(item);
+            return true;
+        }
+    }
+}
